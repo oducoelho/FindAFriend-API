c84a2699cc1b9005e39542337bd44def48c8ef8f
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-08-02 21:04:23 -0300

    integration with PRISMA ORM

---
+++ prisma/migrations/20230802175921_create_organizations_and_pet/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "organizations" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cep" DECIMAL(65,30) NOT NULL,
+    "endereco" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "idade" TEXT NOT NULL,
+    "porte" TEXT NOT NULL,
+    "nivel_de_energia" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "organization_id" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organizations_email_key" ON "organizations"("email");
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.organization.create({
+  data: {
+    cep: '13705000',
+    email: 'jon.doe@example.com',
+    endereco: 'example',
+    name: 'jon doe',
+    password_hash: '123',
+    whatsapp: '199999999999',
+  },
+})
