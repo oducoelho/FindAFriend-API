efc10edfcf85f46172c9b80e61bf19db7b325073
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-08-15 20:47:24 -0300

    controller: profile controller

--- src/http/controllers/org/routes.ts
+++ src/http/controllers/org/routes.ts
@@ -2,10 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { registerAOrg } from './register-a-org'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/org', registerAOrg)
   app.post('/sessions', authenticate)
 
-  app.get('/me', profile)
+  // Authenticate
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
--- src/http/routes.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from 'fastify'
-import { registerAOrg } from './controllers/org/register-a-org'
-import { authenticate } from './controllers/org/authenticate'
-
-export async function appRoutes(app: FastifyInstance) {
-  app.post('/org', registerAOrg)
-  app.post('/sessions', authenticate)
-}
