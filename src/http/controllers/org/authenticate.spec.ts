46ae53a72b7b247f7c750e8642f8f7c81f9673eb
Eduardo Coelho <eduardocoelho619@gmail.com>
2023-08-17 20:29:04 -0300

    Tests E2E: Authenticate E2E Test

--- src/http/controllers/org/authenticate.spec.ts
+++ src/http/controllers/org/authenticate.spec.ts
@@ -1,3 +1,31 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/orgs').send({
+      name: 'ORG1',
+      email: 'ORG@example.COM',
+      cep: '13705000',
+      address: 'addressExample',
+      phone_number: '12345678',
+      password: '123456',
+    })
+    const response = await request(app.server).post('/sessions').send({
+      email: 'ORG@example.COM',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
